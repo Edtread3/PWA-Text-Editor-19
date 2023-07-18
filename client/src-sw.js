db4b533528152098beeb1719ece5114131c4e05b
unknown <edtread3@gmail.com>
2023-07-18 19:03:04 -0400

    made corrections to my connection and server.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require("workbox-strategies");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -23,29 +24,15 @@ warmStrategyCache({
   urls: ["/index.html", "/"],
   strategy: pageCache,
 });
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-const { StaleWhileRevalidate } = require("workbox-strategies");
-
-// Implement asset caching
+registerRoute(({request}) =>request.mode === 'navigate', pageCache);
 registerRoute(
-  ({ request }) =>
-   request.destination === "script" || request.destinaion === "style",
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
-    cacheName: "static-resources",
-  })
-);
-
-registerRoute(
-  ({request}) => request.destination === "image",
-  new CacheFirst({
-    cacheName: "images",
+    cacheName: 'asset-cache',
     plugins: [
-      new ExpirationPlugin({
-        maxEntries:60,
-        maxAgeSeconds: 30 * 24 * 60 * 60,
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
       }),
-    ]
-  }),
+    ],
+  })
 );
--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
+//const htmlRoutes = require('./routes/htmlRoutes');
+const path = require("path")
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use('/', htmlRoutes)
 
-require('./routes/htmlRoutes')(app);
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
